d6c68555cd8dfd40828da17982601892da640958
SumanSantosh <72086164+SumanSantosh@users.noreply.github.com>
2021-05-10 19:49:10 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -17,22 +17,7 @@ function draw() {
   background(255,255,255);  
   if(wall.x-bullet.x < (bullet.width+wall.width)/3){
     bullet.velocityX=0
-    var deformation=0.5*weight*speed*speed/22500;
-    if (deformation>180)
-    {
-      bullet.shapeColor=color(255,0,0);
     
-    }
-    if (deformation>180 && deformation>100)
-    {
-      bullet.shapeColor=color(230,230,0);
-    
-    }
-    if (deformation<100)
-    {
-      bullet.shapeColor=color(0,255,0);
-    
-    }
     if (hasCollided(bullet,wall))
     {
       bullet.velocityX=0;
